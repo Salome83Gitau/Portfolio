86d73f6bbec8c59e36b0951839a22418e7d58ad7
Salome83Gitau <salome83wambui@gmail.com>
2024-09-18 17:57:50 +0300

    first commit

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-// Toggle visibility of the cards container on burger menu click
-document.getElementById("burger-menu").addEventListener("click", function() {
-    const cardsContainer = document.getElementById("cards-container");
+const projectItems = document.querySelectorAll('.project-item');
+const carouselItems = document.querySelectorAll('.carousel-item');
 
-    // If cards container is hidden, show it; otherwise, hide it
-    if (cardsContainer.classList.contains("hidden")) {
-        cardsContainer.classList.remove("hidden");
-        cardsContainer.classList.add("visible");
-    } else {
-        cardsContainer.classList.remove("visible");
-        cardsContainer.classList.add("hidden");
-    }
-});
-
-// Handle card click animations
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', function() {
-        const selectedCard = this;
-
-        // Loop through all cards to manage visibility and the active state
-        document.querySelectorAll('.card').forEach(card => {
-            if (card === selectedCard) {
-                // Activate the selected card (expand it)
-                card.classList.add('active');
-                card.classList.remove('hide');
+projectItems.forEach((item) => {
+    item.addEventListener('mouseover', function() {
+        const projectIndex = this.getAttribute('data-project');
+        carouselItems.forEach((carouselItem, index) => {
+            if (index == projectIndex - 1) {
+                carouselItem.classList.add('active');
             } else {
-                // Hide other cards
-                card.classList.add('hide');
+                carouselItem.classList.remove('active');
             }
         });
     });
